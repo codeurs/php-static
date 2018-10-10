d9454844f21a2172c109fe1f9bce1f9a2d2031dd
Ben Merckx <ben@codeurs.be>
2018-10-10 15:32:54 +0200

    Add root to usage

--- packages/php-serve/cli.js
+++ packages/php-serve/cli.js
@@ -4,10 +4,9 @@ const yargs = require('yargs')
 const phpServe = require('.')
 
 const {port = 80, host = '127.0.0.1', _ = ['.']} = yargs
-  .usage('Usage: $0 -p 80 -h 127.0.0.1 .')
+  .usage('Usage: $0 -p 80 -h 127.0.0.1 ./root')
   .alias('p', 'port')
   .alias('h', 'host')
-  .demandCommand(1)
   .argv
 
-  phpServe(host, port, _[0])
+  phpServe(host, port, _[0] || '.')
