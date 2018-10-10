10734f73ddbdfdca582b7c4403888ca57256e810
Ben Merckx <ben@codeurs.be>
2018-10-10 17:04:44 +0200

    php-serve v0.0.1-alpha.3

--- packages/php-serve/cli.js
+++ packages/php-serve/cli.js
@@ -6,7 +6,6 @@ const phpServe = require('.')
 const {port = 80, host = '127.0.0.1', _ = ['.']} = yargs
   .usage('Usage: $0 -p 80 -h 127.0.0.1 ./root')
   .alias('p', 'port')
-  .alias('h', 'host')
-  .argv
+  .alias('h', 'host').argv
 
-  phpServe(host, port, _[0] || '.')
+phpServe(host, port, _[0] || '.')
